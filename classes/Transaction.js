7bef1671ddb8a0557fd1c31067a958bc2ae3a481
Ahmed Sayed <>
2024-11-27 16:36:44 +0200

    safdasf

--- classes/Transaction.js
+++ classes/Transaction.js
@@ -217,7 +217,9 @@ class Transaction extends DB {
       if(!this._price || !this._baseAmountIn || !this.ticker.currentPrice || this._status === "CLOSED") return;
       if(this.position === null) this.position = this.ticker.currentPrice > this._price ? "LOWER" : "HIGHER";
       this._takeProfit = this._side === "LONG" ? this._price + this.trader._takeProfit : this._price  - this.trader._takeProfit;
-      this._stopLoss = this.trader._stopLoss > 0 ? this._side === "LONG" ? this._price - this.trader._stopLoss : this._price + this.trader._stopLoss : 0;
+      this._stopLoss = this._side === "LONG" ? this._price - (this.trader._stopLoss > 0 ? this.trader._stopLoss : ((this._price / this.trader._leverage) * 0.9)) : this._price + (this.trader._stopLoss > 0 ? this.trader._stopLoss : ((this._price / this.trader._leverage) * 0.9));
+
+      console.log("STOP LOSS:::::", this._stopLoss);
 
       if(this._mode === "TESTING" && this._status !== "CLOSED") {
         if(
