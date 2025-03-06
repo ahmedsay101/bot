8f0c1c249165d662c494411f5eb89422a8815ffd
Ahmed Sayed <ahmed.sayed@devteam.bg>
2025-03-06 02:00:44 +0200

    fix

--- classes/Trader.js
+++ classes/Trader.js
@@ -343,10 +343,10 @@ class Trader extends DB {
                 const longAmount = this.transactions.filter(obj => obj._price === longLevel).map(obj => Number(obj._baseAmountIn)).reduce((total, current) => total + current);
                 const shortAmount = this.transactions.filter(obj => obj._price === shortLevel).map(obj => Number(obj._baseAmountIn)).reduce((total, current) => total + current);
 
-                if(this.transactions.filter(obj => Number(obj._baseAmountIn) >= Number(this._maxBaseAmountIn)).length > 1) {
+                /*if(this.transactions.filter(obj => Number(obj._baseAmountIn) >= Number(this._maxBaseAmountIn)).length > 1 && this._maxBaseAmountIn > 0) {
                     await this.revive();
                     return;
-                }
+                }*/
 
                 if(isAllLongFilled && (shortAmount <= longAmount)) {
                     const lastBaseAmountIn = this.transactions.filter(obj => obj._price === longLevel).sort((a, b) => b._baseAmountIn - a._baseAmountIn)[0]?._baseAmountIn;
