62ed46ed6d52b43ce1d47a76cc72f2989e200fc1
Ahmed Sayed <ahmed.sayed@devteam.bg>
2025-04-13 02:58:31 +0200

    fx

--- classes/Trader.js
+++ classes/Trader.js
@@ -129,7 +129,7 @@ class Trader extends DB {
 
     async isMarketActive() {
         try {
-            const candles = await this.service.getKlines(this.symbol, limit = 30, interval = "5m");
+            const candles = await this.service.getKlines(this.symbol, 30, "5m");
             const threshold = 200;
         
             let ranges = [];
