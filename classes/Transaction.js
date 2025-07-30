83be0d0bb01bb86b1a486ed040d63040af943a3d
Ahmed Sayed <ahmed.sayed@devteam.bg>
2025-07-30 20:39:09 +0300

    fxz

--- classes/Transaction.js
+++ classes/Transaction.js
@@ -53,7 +53,7 @@ class Transaction extends DB {
     try {
       //if(this.trader._currentRounds === 1) this._baseAmountIn = this.trader._baseAmountIn * this.trader._doubles;
       this._baseAmountIn = this.trader._currentAmountIn;
-      if(this._baseAmountIn > this.trader._peakBaseAmountIn) this.trader._peakBaseAmountIn = this._baseAmountIn;
+      if(this._baseAmountIn > this.trader._peakBaseAmountIn && !this._isFake) this.trader._peakBaseAmountIn = this._baseAmountIn;
       if(this._mode === "LIVE" && !this._isFake) {
         await this.order();
       }
