6b0fa8ca14dcd601070d918750f95a7f3a280bc6
Ahmed Sayed <ahmed.sayed@devteam.bg>
2025-08-05 22:53:20 +0300

    سف

--- classes/MarketDetector.js
+++ classes/MarketDetector.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 class MarketActivityDetector {
   constructor(symbol = 'UNIUSDT', opts = {}) {
     this.symbol = symbol;
-    this.mode = opts.mode || 'medium';
+    this.mode = opts.mode || 'strict';
     this.client = axios.create({ baseURL: 'https://fapi.binance.com' });
 
     this.wsKline = null;
