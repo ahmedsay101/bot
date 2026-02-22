a103ca8e090ffef8375dd843507fc0cc7814cbc3
Ahmed Sayed <ahmed.sayed@devteam.bg>
2026-02-22 19:37:36 +0200

    30

--- src/utils/config.js
+++ src/utils/config.js
@@ -11,7 +11,7 @@ const config = {
   slippageRate: 0.0002,
   riskPerTradeUSDT: 20,
   positionNotionalUSDT: 300,
-  equityFraction: 0.40,
+  equityFraction: 0.30,
   leverage: 10,
   levelCount: 2,
   levelSpacingPercent: 3,
